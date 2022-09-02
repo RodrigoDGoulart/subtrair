87541cbf3912930506b8e35cf394f88eb0291d65
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2022-09-02 10:07:44 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Subtrador {
+    subtrair(n1, n2) {
+        return n1 - n2;
+    }
+}
+exports.default = Subtrador;
